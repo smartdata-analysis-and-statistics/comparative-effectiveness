4ed80888f5a3298a415e082b0b72b75c9a78c142
Thomas Debray <117118104+tdebray123@users.noreply.github.com>
2023-11-24 10:49:59 +0100

    update figures

--- resources/chapter 12/fig_functions.r
+++ resources/chapter 12/fig_functions.r
@@ -3,62 +3,76 @@ library(dplyr)
 
 plot_distribution_progression <- function(sim_data) {
   sim_data <- subset(sim_data, time %% 3 == 0)
-  
-  
-  ggdat <- sim_data %>% 
-    group_by(x, time) %>% 
-      dplyr::summarize(perc = sum(progression) / n()) 
+
+
+  ggdat <- sim_data %>%
+    group_by(x, time) %>%
+      dplyr::summarize(perc = sum(progression) / n())
   ggdat$Treatment <- factor(ggdat$x, levels = c(0,1), labels = c("DMT A", "DMT B"))
   ggdat$time <- as.factor(ggdat$time)
-  
+
   # Describe the complete data
-  g <- ggplot(ggdat, aes(x = time, y = perc, fill = Treatment)) + 
+  g <- ggplot(ggdat, aes(x = time, y = perc, fill = Treatment)) +
     geom_bar(stat = "identity", position = position_dodge()) +
     ggtitle("EDSS Progression from baseline") +
     xlab("Treatment exposure (no. months)") +
-    ylab("Risk of EDSS progression") + 
+    ylab("Risk of EDSS progression") +
     scale_fill_brewer(palette = "Blues")
   return(g)
 }
 
-plot_distribution_edss <- function(sim_data) {
-  sim_data <- subset(sim_data, time %% 3 == 0)
+ggplot_distribution_edss <- function(sim_data) {
+  require(ggplot2)
+
   sim_data$Treatment <- factor(sim_data$x, levels = c(0,1), labels = c("DMT A", "DMT B"))
   sim_data$time <- as.factor(sim_data$time)
-  
-  # Describe the complete data
-  g <- ggplot(sim_data, aes(x = time, y = y, fill = Treatment)) + 
-    geom_boxplot(outlier.shape = NA, notch = TRUE) +
-    ggtitle("EDSS Prognosis") +
-    xlab("Treatment exposure (no. months)") +
-    ylab("Expanded Disability Status Scale") + 
-    scale_fill_brewer(palette = "Blues")
-  return(g)
+
+  simsum <- sim_data %>% group_by(time, Treatment) %>% summarize(
+    median = median(y),
+    mean = mean(y),
+    lqd = quantile(y)["25%"],
+    uqd = quantile(y)["75%"],
+    miny = min(y),
+    maxy = max(y),
+    month = as.numeric(as.character(time))[1]
+  )
+
+  ggplot(simsum, aes(x = month, y = median, group = Treatment)) +
+    geom_line() +
+    geom_point(aes(y=miny)) +
+    geom_point(aes(y=maxy)) +
+    geom_line(aes(y=lqd), lty = 2, color = "grey") +
+    geom_line(aes(y=uqd), lty = 2, color = "grey") +
+    geom_ribbon(aes(ymin = lqd, ymax = uqd), fill = "grey", alpha = 0.5) +  # Shaded area
+    xlab("Treatment exposure (months)") +
+    ylab("Median EDSS") +
+    facet_wrap(~ Treatment)  +
+    theme_bw()
 }
 
 
 
 plot_prob_visit <- function(sim_data) {
-  
+
   sim_data$Treatment <- factor(sim_data$x, levels = c(0,1), labels = c("DMT A", "DMT B"))
-  
-  dat_visits <- sim_data %>% group_by(patid) %>% 
+
+  dat_visits <- sim_data %>% group_by(patid) %>%
     dplyr::summarize(x = unique(x),
               nvisits_theory = sum(prob_yobs),
               nvisits_empirical = sum(!is.na(y_obs)))
-  
+
   dat_text <- data.frame(
     label = paste("Simulated visits per patient: ",
-                  round(c(mean(subset(dat_visits, x == 0)$nvisits_theory), 
+                  round(c(mean(subset(dat_visits, x == 0)$nvisits_theory),
                           mean(subset(dat_visits, x == 1)$nvisits_theory)),1)),
     Treatment = unique(sim_data$Treatment)[order(unique(sim_data$x))]
   )
-  
-  g <- ggplot(sim_data, aes(x = time, y = prob_yobs)) + 
-    geom_boxplot(aes(group = time, fill = Treatment), outlier.shape = NA) + 
+
+  g <- ggplot(sim_data, aes(x = time, y = prob_yobs)) +
+    geom_boxplot(aes(group = time, fill = Treatment), outlier.shape = NA) +
     ylab("Probability of a patient visit") +
-    xlab("Treatment exposure (no. months)") + 
-    facet_wrap(~Treatment) + 
+    xlab("Treatment exposure (no. months)") +
+    facet_wrap(~Treatment) +
     geom_text(
       data    = dat_text,
       mapping = aes(x = Inf, y = Inf, label = label),
@@ -67,6 +81,6 @@ plot_prob_visit <- function(sim_data) {
     ) +
     scale_fill_brewer(palette = "Blues") +
     theme(legend.position = "bottom")
-  
+
   return(g)
 }
